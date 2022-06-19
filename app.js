074f5d12abf2a77b57ceb1c2ea41ebf7d6ad73b0
FrankFMY <pryanishnikovartem@gmail.com>
2022-06-19 17:16:19 +0300

    Step 8

--- app.js
+++ app.js
@@ -1,19 +1,66 @@
 $(function () {
 
+
+    /* Fixed Header */
     let header = $("#header");
     let intro = $("#intro");
-    let introH;
+    let introH = intro.innerHeight();
     let scrollPos = $(window).scrollTop();
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
 
-    $(window).on("scroll load resize", function () {
+    checkScroll(scrollPos, introH);
+
+    $(window).on("scroll resize", function () {
         introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
 
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
         if (scrollPos > introH) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
+    }
+
+
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        }, 700);
     });
+
+
+    /* Nav Toggle */
+    navToggle.on("click", function (event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+
+
+    /* Reviews: https://kenwheeler.github.io/slick/ */
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+    });
+
 
 });
